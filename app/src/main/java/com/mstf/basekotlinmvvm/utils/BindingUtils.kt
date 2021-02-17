b10d3f560fa0773f8faa4076277f90741ca49eef
Mostafa <imostafa70@gmail.com>
2021-02-17 12:21:10 +0330

    first commit

---
+++ app/src/main/java/com/mstf/basekotlinmvvm/utils/BindingUtils.kt
@@ -0,0 +1,25 @@
+package com.mstf.basekotlinmvvm.utils
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+
+object BindingUtils {
+
+    /*
+    @BindingAdapter("adapter")
+    fun addBlogItems(recyclerView: RecyclerView, blogs: List<BlogResponse.Blog>?) {
+        val adapter: BlogAdapter = recyclerView.adapter as BlogAdapter
+        if (adapter != null) {
+            adapter.clearItems()
+            adapter.addItems(blogs)
+        }
+    }
+    */
+
+    @BindingAdapter("imageUrl")
+    fun setImageUrl(imageView: ImageView, url: String) {
+        val context = imageView.context
+        Glide.with(context).load(url).into(imageView)
+    }
+}
